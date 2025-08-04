623005e3761a3ad534f4ba30bbf846bbf359b00b
HsuJinho <hsujinho@gmail.com>
2025-08-04 16:18:20 +0800

    docs: add class docs

--- Assets/Scripts/Game/StateMachine.cs
+++ Assets/Scripts/Game/StateMachine.cs
@@ -7,48 +7,43 @@ namespace Game
     using HZI.Shared.Interfaces;
     using System.Collections.Generic;
     
+    /// <summary>
+    /// 狀態機元件，負責管理當前狀態節點並處理狀態切換事件。
+    /// </summary>
     public class StateMachine: MonoBehaviour
     {
-        // private StateNode _currentStateNode;
-        // public Dictionary<Type, StateNode> StateNodes;
-        // public HashSet<ITransition> AnyTransitions;
-        //
-        // public void changeState(IState state)
-        // {
-        //     if(state == _currentStateNode.state)
-        //         return;
-        //     
-        //     var previousState = _currentStateNode.state;
-        //     var nextState = StateNodes[state.GetType()].state;
-        //     
-        //     previousState.OnExit();
-        //     nextState.OnEnter();
-        //     _currentStateNode = StateNodes[state.GetType()];
-        // }
-        
-        // private IState _currentState;
         private StateNode _currentStateNode;
         [SerializeField] private EventChannel transitionEventChannel;
 
+        /// <summary>
+        /// 啟用時訂閱狀態轉換事件。
+        /// </summary>
         private void OnEnable()
         {
             if (transitionEventChannel != null) transitionEventChannel.OnEventRaised += HandleEventRaised;
         }
 
+        /// <summary>
+        /// 停用時取消訂閱狀態轉換事件。
+        /// </summary>
         private void OnDisable()
         {
             if (transitionEventChannel != null) transitionEventChannel.OnEventRaised -= HandleEventRaised;       
         }
         
+        /// <summary>
+        /// 狀態轉換事件處理方法，執行事件邏輯。
+        /// </summary>
+        /// <param name="raisedEvent">被觸發的事件。</param>
         private void HandleEventRaised(IEvent raisedEvent)
         {
             raisedEvent.Execute();
         }
-
+        
         /// <summary>
-        /// Changes the current state of the state machine to the specified state node.
+        /// 切換至指定的狀態節點，並執行離開與進入狀態的相關邏輯。
         /// </summary>
-        /// <param name="toStateNode">The target state node to transit to.</param>
+        /// <param name="toStateNode">目標狀態節點。</param>
         public void ChangeState(StateNode toStateNode)
         {
             if (toStateNode == _currentStateNode || toStateNode == null)
--- Assets/Scripts/Shared/Data/Events/StateTransitionEvent.cs
+++ Assets/Scripts/Shared/Data/Events/StateTransitionEvent.cs
@@ -1,8 +1,13 @@
 using HZI.Shared.Interfaces;
 using UnityEngine.Events;
+using Game;
 
-namespace Game
+namespace HZI.Shared.Data.Events
 {
+    /// <summary>
+    /// 狀態轉換事件，繼承自 UnityEvent 並實作 IEvent 介面。
+    /// 用於在狀態機中觸發狀態節點的轉換。
+    /// </summary>
     [System.Serializable]
     public class StateTransitionEvent: UnityEvent, IEvent
     {
@@ -15,6 +20,9 @@ namespace Game
             _stateMachine = stateMachine;
         }
 
+        /// <summary>
+        /// 執行狀態轉換邏輯，若有有效轉換則切換狀態。
+        /// </summary>
         public void Execute()
         {
             var transition = _stateNode.GetValidTransition();
